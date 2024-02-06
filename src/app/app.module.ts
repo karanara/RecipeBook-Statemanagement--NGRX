8e26746e5756507532f29e700016a78354af3eb1
karanara <karanara@mail.uc.edu>
2024-02-05 22:06:51 -0500

    auth,shopping -ngrx

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+import * as fromApp from './store/app.reducer';
+import { EffectsModule } from '@ngrx/effects';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    StoreModule.forRoot(fromApp.appReducer),
+    SharedModule,
+    CoreModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+  ],
+  bootstrap: [AppComponent]
+  // providers: [LoggingService]
+})
+export class AppModule {}
